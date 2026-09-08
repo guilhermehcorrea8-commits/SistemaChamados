f1f574d289a644197db1134fa8e496afe5947c70
guilhermehcorrea8 <guilherme.h.correa8@aluno.senai.br>
2026-09-08 09:25:23 -0300

    configura entity framework e contexto do banco

---
+++ src/backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Chamado> Chamados { get; set; }
+}
